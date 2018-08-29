de3456e5970fc650b39bbe26692b7cffc3dcf904
thisnameisprivate <2632798785@qq.com>
2018-08-29 10:24:53 +0800

    单例模式if逻辑写错了

--- clearPhpComment.php
+++ clearPhpComment.php
@@ -3,30 +3,30 @@
  *  清除php文件注释和多余的空格
  *
  * */
-// open on need comment php file Header to clear.
-$phpCode =  php_strip_whitespace('D:\wampserver\wamp\www\ThinkPHP\App\Home\Controller\IndexController.class.php');
-// create file.
-$myfile = fopen("IndexController.class.php", "w") or die("Unable to open file!");
-// write new phpcode file.
-fwrite($myfile, $phpCode);
-// close header resource link
-fclose($myfile);
+
 
 class Something {
     private static $instance = null;
+    private $name = 'This is instance Test';
     private function __construct()
     {
 
     }
-    public static function instance ()
+    public static function instance ($methods)
     {
         if (! self::$instance == null) {
-            return self::$instance = new Something;
-        } else {
             return self::$instance;
+        } else {
+            print_r($methods);
+            return self::$instance = new Something;
         }
     }
+    public function getMethods ()
+    {
+        return $this->name;
+    }
 }
-
-$something = Something::instance();
-var_dump($something);
+$methods = 'Hello, GirlFriend.';
+$something = Something::instance($methods);
+$result = $something->getMethods();
+print_r($result);
